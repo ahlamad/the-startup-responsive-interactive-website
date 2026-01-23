5528167d7ae4bbf855465593b52d1246e1f867db
ahlamad <ahlam.adda@hva.nl>
2026-01-23 23:00:49 +0100

    #9 clickevents to adminbuttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 // Buttons & forms in variabelen stoppen
+const hostButton = document.getElementById('host-button');
+const guestButton = document.getElementById('guest-button');
+
+const hostForm = document.querySelector('host-notifications');
+const guestForm = document.querySelector('guest-notifications');
 
 // Clickevent op de buttons 
+hostButton.addEventListener('click', showHostNotifications);
+guestButton.addEventListener('click', showGuestNotifications);
 
 // 2 functies voor het tonen van gast- en verhuurdernotifcaties

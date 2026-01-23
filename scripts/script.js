1e9d23c03d9cc3df3a613b565d65d206a519ea22
ahlamad <ahlam.adda@hva.nl>
2026-01-23 23:08:59 +0100

    close #9 adminrollen done

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,26 @@
 const hostButton = document.getElementById('host-button');
 const guestButton = document.getElementById('guest-button');
 
-const hostForm = document.querySelector('host-notifications');
-const guestForm = document.querySelector('guest-notifications');
+const hostForm = document.querySelector('.host-notifications');
+const guestForm = document.querySelector('.guest-notifications');
 
 // Clickevent op de buttons 
 hostButton.addEventListener('click', showHostNotifications);
 guestButton.addEventListener('click', showGuestNotifications);
 
 // 2 functies voor het tonen van gast- en verhuurdernotifcaties
+function showHostNotifications() {
+    hostForm.classList.remove('hidden');
+    guestForm.classList.add('hidden');
+
+    hostButton.classList.add('active');
+    guestButton.classList.remove('active');
+}
+
+function showGuestNotifications() {
+    guestForm.classList.remove('hidden');
+    hostForm.classList.add('hidden');
+
+    guestButton.classList.add('active');
+    hostButton.classList.remove('active');
+}
